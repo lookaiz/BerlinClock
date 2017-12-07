c480024159e3964b19fe38beb05ca570fe90452a
lookaiz <mmonchy@palo-it.com>
2017-12-07 22:06:43 +0100

    Initial commit

---
+++ js/BerlinClock.js
@@ -0,0 +1,81 @@
+var BerlinClock = function(h, m, s) {
+	this.time = new Date(0,0,0, h, m, s);
+}
+
+var BerlinClock = function() {
+
+}
+
+BerlinClock.prototype.setTime = function(time) {
+    var timeElements = time.split(':');
+    if (timeElements.length != 3) {
+        throw new Error('Invalid time value: ' + time);
+    }
+
+    var hour = timeElements[0];
+    var minute = timeElements[1];
+    var seconds = timeElements[2];
+
+    if(Number.isNaN(parseInt(hour)) || Number.isNaN(parseInt(minute)) || Number.isNaN(parseInt(seconds)) ) {
+        throw new Error('Invalid time value: ' + time);
+    }
+
+    if((hour > 23) || (minute > 59) || (seconds > 59)) {
+        throw new Error('Invalid time value: ' + time);
+    }
+    this.time = new Date(0, 0, 0, hour, minute, seconds);
+}
+
+BerlinClock.prototype.getSeconds = function() {
+    var seconds = this.time.getSeconds();
+    if(seconds % 2 === 1) {
+        return 'Y';
+    }
+    return 'O';
+}
+
+BerlinClock.prototype.getFiveHoursRow = function() {
+    var fiveHours = ['O', 'O', 'O', 'O'];
+    var h = this.time.getHours();
+    var nbFiveHours = (h - h%5)/5;
+    for (i = 0; i < nbFiveHours; i++) {
+        fiveHours[i] = 'R';
+    }
+    return fiveHours;
+}
+
+BerlinClock.prototype.getOneHoursRow = function() {
+    var oneHours = ['O', 'O', 'O', 'O'];
+    var h = this.time.getHours();
+    var nbOneHours = h%5;
+    for (i = 0; i < nbOneHours; i++) {
+        oneHours[i] = 'R';
+    }
+    return oneHours;
+}
+
+BerlinClock.prototype.getFiveMinutesRow = function() {
+    var fiveMinutes = ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'];
+    var m = this.time.getMinutes();
+    var nbFiveMinutes = (m - m%5)/5;
+    var redArrayIndexes = [2, 5, 8];
+    for (i = 0; i < nbFiveMinutes; i++) {
+        if(redArrayIndexes.includes(i)) {
+            fiveMinutes[i] = 'R';
+        }
+        else {
+            fiveMinutes[i] = 'Y';
+        }
+    }
+    return fiveMinutes;
+}
+
+BerlinClock.prototype.getOneMinutesRow = function() {
+    var oneMinutes = ['O', 'O', 'O', 'O'];
+    var m = this.time.getMinutes();
+    var nbOneMinutes = m%5;
+    for (i = 0; i < nbOneMinutes; i++) {
+        oneMinutes[i] = 'Y';
+    }
+    return oneMinutes;
+}
---
+++ specs/BerlinClockSpec.js
@@ -0,0 +1,145 @@
+describe("BerlinClock", function(){
+	
+	beforeEach(function() {
+		this.clock = new BerlinClock();
+	});
+
+	describe('error cases', function() {
+		it('null time', function() {
+			expect(this.clock.setTime.bind(this.clock, null)).toThrowError(Error);
+		});
+
+		it('empty time', function() {
+			expect(this.clock.setTime.bind(this.clock, "")).toThrowError(Error);
+		});
+
+		it('invalid hour value', function() {
+			expect(this.clock.setTime.bind(this.clock, "AA:13:56")).toThrowError(Error);
+		});
+
+		it('invalid minute value', function() {
+			expect(this.clock.setTime.bind(this.clock, "02:F:53")).toThrowError(Error);
+		});
+
+		it('invalid second value', function() {
+			expect(this.clock.setTime.bind(this.clock, "09:21:%")).toThrowError(Error);
+		});
+
+		it('only hour', function() {
+			expect(this.clock.setTime.bind(this.clock, "18")).toThrowError(Error);
+		});
+
+		it('only hour and minutes', function() {
+			expect(this.clock.setTime.bind(this.clock, "17:45")).toThrowError(Error);
+		});
+
+		it('hour > 23', function() {
+			expect(this.clock.setTime.bind(this.clock, "30:22:14")).toThrowError(Error);
+		});
+
+		it('minutes > 59', function() {
+			expect(this.clock.setTime.bind(this.clock, "12:77:14")).toThrowError(Error);
+		});
+
+		it('seconds > 59', function() {
+			expect(this.clock.setTime.bind(this.clock, "12:22:65")).toThrowError(Error);
+		});
+
+	});
+
+	describe('seconds', function() {
+		it('odd seconds', function() {
+			this.clock.setTime("23:42:59");
+			expect(this.clock.getSeconds()).toEqual('Y');
+		});
+
+		it('even seconds', function() {
+			this.clock.setTime("23:42:58");
+			expect(this.clock.getSeconds()).toEqual('O');
+		});
+	});
+
+	describe('five-hours', function() {
+		it('00:00:00', function() {
+			this.clock.setTime("00:00:00");
+			expect(this.clock.getFiveHoursRow()).toEqual(['O', 'O', 'O', 'O']);
+		});
+
+		it('23:59:59', function() {
+			this.clock.setTime("23:59:59");
+			expect(this.clock.getFiveHoursRow()).toEqual(['R', 'R', 'R', 'R']);
+		});
+
+		it('02:04:00', function() {
+			this.clock.setTime("02:04:00");
+			expect(this.clock.getFiveHoursRow()).toEqual(['O', 'O', 'O', 'O']);
+		});
+
+		it('08:23:00', function() {
+			this.clock.setTime("08:23:00");
+			expect(this.clock.getFiveHoursRow()).toEqual(['R', 'O', 'O', 'O']);
+		});
+
+		it('16:35:00', function() {
+			this.clock.setTime("16:35:00");
+			expect(this.clock.getFiveHoursRow()).toEqual(['R', 'R', 'R', 'O']);
+		});
+	});
+
+	describe('one-hours', function() {
+		
+		it('00:00:00', function() {
+			this.clock.setTime("00:00:00");
+			expect(this.clock.getOneHoursRow()).toEqual(['O', 'O', 'O', 'O']);
+		});
+
+		it('23:59:59', function() {
+			this.clock.setTime("23:59:59");
+			expect(this.clock.getOneHoursRow()).toEqual(['R', 'R', 'R', 'O']);
+		});
+
+		it('02:04:00', function() {
+			this.clock.setTime("02:04:00");
+			expect(this.clock.getOneHoursRow()).toEqual(['R', 'R', 'O', 'O']);
+		});
+
+		it('08:23:00', function() {
+			this.clock.setTime("08:23:00");
+			expect(this.clock.getOneHoursRow()).toEqual(['R', 'R', 'R', 'O']);
+		});
+
+		it('14:35:00', function() {
+			this.clock.setTime("14:35:00");
+			expect(this.clock.getOneHoursRow()).toEqual(['R', 'R', 'R', 'R']);
+		});
+	});
+
+	describe('five-minutes', function() {
+		
+		it('00:00:00', function() {
+			this.clock.setTime("00:00:00");
+			expect(this.clock.getFiveMinutesRow()).toEqual(['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O']);
+		});
+
+		it('23:59:59', function() {
+			this.clock.setTime("23:59:59");
+			expect(this.clock.getFiveMinutesRow()).toEqual(['Y', 'Y', 'R', 'Y', 'Y', 'R', 'Y', 'Y', 'R', 'Y', 'Y']);
+		});
+
+		it('12:04:00', function() {
+			this.clock.setTime("12:04:00");
+			expect(this.clock.getFiveMinutesRow()).toEqual(['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O']);
+		});
+
+		it('12:23:00', function() {
+			this.clock.setTime("12:23:00");
+			expect(this.clock.getFiveMinutesRow()).toEqual(['Y', 'Y', 'R', 'Y', 'O', 'O', 'O', 'O', 'O', 'O', 'O']);
+		});
+
+		it('12:35:00', function() {
+			this.clock.setTime("12:35:00");
+			expect(this.clock.getFiveMinutesRow()).toEqual(['Y', 'Y', 'R', 'Y', 'Y', 'R', 'Y', 'O', 'O', 'O', 'O']);
+		});
+	});
+
+});
